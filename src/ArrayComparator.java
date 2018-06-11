d192a0e91f158e234b66670ba59c2f97a704e4af
EwelinaGordon <ewelina.gordon@gmail.com>
2018-06-11 21:28:44 +0200

    Zadanie 6.3 Uwzględniłam uwagi

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -1,24 +1,25 @@
 public class ArrayComparator {
 
     boolean compare(int[][] table1, int[][] table2) {
-        boolean diff = true;
+        if (table1 == null && table2 == null) {
+            return true;
+        }
 
         if ((table1 != null && table2 == null) || (table1 == null && table2 != null)) {
-            diff = false;
+            return false;
         }
 
         if (table1.length != table2.length) {
-            diff = false;
+            return false;
         }
 
-        while (diff) {
-            for (int i = 0; i < table1.length; i++) {
-                for (int j = 0; j < table1[1].length; j++) {
-                    diff = (table1[i][j] == table2[i][j]);
+        for (int i = 0; i < table1.length; i++) {
+            for (int j = 0; j < table1[1].length; j++) {
+                if (table1[i][j] != table2[i][j]) {
+                    return false;
                 }
             }
-            break;
         }
-        return diff;
+        return true;
     }
 }
--- src/ArrayComparatorTest.java
+++ src/ArrayComparatorTest.java
@@ -1,19 +1,14 @@
 public class ArrayComparatorTest {
     public static void main(String[] args) {
 
-
         int[][] tab1 = new int[2][2];
         int[][] tab2 = new int[2][2];
-        tab1[0] = new int[]{2, 2};
+        tab1[0] = new int[]{2, 1};
         tab1[1] = new int[]{2, 2};
         tab2[0] = new int[]{2, 2};
         tab2[1] = new int[]{2, 2};
 
         ArrayComparator arrayComparator = new ArrayComparator();
-
-
         System.out.println(arrayComparator.compare(tab1, tab2));
-
-
     }
 }
